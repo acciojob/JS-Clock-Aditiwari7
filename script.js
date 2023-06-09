18aa233d53e01c24b181f2bee32e2829b18d19c6
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-06-09 19:04:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your code here
+const hourHand = document.querySelector(".hour-hand");
+const minHand = document.querySelector(".min-hand");
+const secHand = d0cument.querySelector(".second-hand");
+
+function setTime() {
+	var time = new Date();
+	const hr = time.getHours();
+	const min = time.getMinutes();
+	const sec = time.getSeconds();
+
+	const hrRot = (hr*30) + (min/2);
+	const minRot = (min*6) + (sec/10);
+	const secRot = sec*6;
+
+	hourHand.style.transform = `rotate(${hrRot})`;
+	minHand.style.transform = `rotate(${minRot})`;
+	secHand.style.transform = `rotate(${secRot})`;
+}
+
+setTime();
+setInterval(setTime, 1000);
